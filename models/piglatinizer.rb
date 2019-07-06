88dd8d6b2feaab2dfda2a3054a3b0ce71137f5e8
Donna Amos <donnacamos.developer@gmail.com>
2019-07-06 18:31:12 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,18 +1,13 @@
-class PigLatinizer
-  def to_pig_latin(phrase)
-    words = phrase.split(" ")
-    words.collect {|w| piglatinize(w)}.join(" ")
+require_relative 'config/environment'
+
+class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
   end
 
-  def piglatinize(word)
-    parts_of_word = word.split(/([^aeiouAEIOU]*)([aeiouAEIOU]*)(.*)/)
-    # binding.pry
-    start = parts_of_word[1] # consonant cluster
-    rest = parts_of_word[2] + (parts_of_word[3] || "")
-    if start.length>0
-      "#{rest}#{start}ay"
-    else
-      "#{rest}way"
-    end
+  post '/piglatinize' do
+    translator = PigLatinizer.new
+    translator.to_pig_latin(params[:user_phrase])
   end
 end
